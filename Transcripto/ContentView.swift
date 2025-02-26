724f8de9d6396be2a544ade3661694e13003f03e
satvikjadhav <satvikj5@gmail.com>
2025-02-25 22:52:40 -0600

    Initial Commit

---
+++ Transcripto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Transcripto
+//
+//  Created by Satvik  Jadhav on 2/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Transcripto/TranscriptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscriptoApp.swift
+//  Transcripto
+//
+//  Created by Satvik  Jadhav on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct TranscriptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
