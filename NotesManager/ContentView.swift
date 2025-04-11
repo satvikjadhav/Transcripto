9d5ea46720e64d433bbd70b045b00e73a2f876fc
Clayton Nunley <clayton.nunley@okstate.edu>
2025-04-10 22:22:43 -0500

    Initial Commit

---
+++ NotesManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotesManager
+//
+//  Created by Clayton Nunley on 4/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotesManager/NotesManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesManagerApp.swift
+//  NotesManager
+//
+//  Created by Clayton Nunley on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NotesManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
